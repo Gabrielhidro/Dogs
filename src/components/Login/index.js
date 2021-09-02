7fa8f82b316029b57386c60bf8e11b0e6ee1c36b
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-09-02 16:50:05 -0300

    Create login routes

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+import CreateAccount from "./components/CreateAccount.js/index.js";
+import ForgotPassword from "./components/ForgotPassword/index.js";
+import LoginForm from "./components/LoginForm.js";
+import ResetPassword from "./components/ResetPassword/index.js";
+
 export default function Login() {
-    return (
-        <div>
-            login
-        </div>
-    )
+  return (
+    <div>
+      <Switch>
+        <Route path="/">
+          <LoginForm />
+        </Route>
+        <Route path="create">
+          <CreateAccount />
+        </Route>
+        <Route path="forgot">
+          <ForgotPassword />
+        </Route>
+        <Route path="reset">
+          <ResetPassword />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
-  
