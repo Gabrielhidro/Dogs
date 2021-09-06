90d9c48ee03b52b95b9343d0869e5264f838284c
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-09-06 14:31:58 -0300

    create api component

--- src/components/Login/components/LoginForm.js/index.js
+++ src/components/Login/components/LoginForm.js/index.js
@@ -1,28 +1,42 @@
 import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
+import { TOKEN_POST } from "../../../../api";
 import useForm from "../../../../shared/hooks/useForm";
 import Button from "../../../Form/Button";
 import Input from "../../../Form/Input";
 
 export default function LoginForm() {
+  const username = useForm("");
+  const password = useForm("");
 
-  const username = useForm('email')
-  const password = useForm('')
+  async function handleSubmitLogin(event) {
+    event.preventDefault();
 
-  async function handleSubmitLogin(event){
-    event.preventDefault()
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
 
-    
-    const response = await axios.post('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      password: password,
-      username: username
-    })
-    const data = response.data
-    console.log(data);
-    return response
+      fetch(url, options)
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
+
+    // const response = await axios.post('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+    //   password: password,
+    //   username: username
+    // })
+    // const data = response.data
+    // console.log(data);
+    // return response
   }
-  
+
   return (
     <div>
       <h1>Login</h1>
@@ -35,26 +49,6 @@ export default function LoginForm() {
       </form>
 
       <Link to="/login/create">Cadastro</Link>
-
     </div>
   );
 }
-
-
-
-
-
-
-
-// fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({ username, password })
-// }).then(response => {
-//   console.log(response)
-//   return response.json()
-// }).then(json => {
-//   console.log(json)
-// })
