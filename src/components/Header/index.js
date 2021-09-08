199691908bd1aadb71062b20b33a4caedc517e69
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-09-08 10:20:25 -0300

    Add user account header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,29 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../context";
 import { DogsLogo } from "../../shared/assets";
 import { Container, Login, Logo, Navigation } from "./styles";
 
 export default function Header() {
+
+  const {data} = React.useContext(UserContext)
+
   return (
     <Container>
         <Navigation>
           <Link to="/">
             <Logo src={DogsLogo} alt="logo" />
           </Link>
-          <Link to="/login">
-            <Login>Login</Login>
-          </Link>
+          {data ? (
+            <Link to="/conta">
+              <Login>{data.nome}</Login>
+            </Link>
+            ):(
+              <Link to="/login">
+                <Login>Login</Login>
+              </Link>
+            )
+          }
         </Navigation>
     </Container>
   )
